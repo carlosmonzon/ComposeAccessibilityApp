649aa7a8011784740110a8bcbb64c30e25fcb72c
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2022-01-05 16:01:41 +1100

    - Adding list custom actions

---
+++ app/src/main/java/com/monzon/accesbilityapp/home/mergeelement/CustomActions.kt
@@ -0,0 +1,140 @@
+package com.monzon.accesbilityapp.home.mergeelement
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import com.monzon.accesbilityapp.R
+
+
+fun LazyListScope.postMetadataList(
+    isAccessibilityEnabled: Boolean = false,
+    items: List<PostMetadata>,
+    onToggleFavourite: (PostMetadata) -> Unit
+) {
+    items(items) { post ->
+        if (isAccessibilityEnabled) {
+            AcsPostMetadataItem(post, onToggleFavourite = {
+                onToggleFavourite(post)
+            })
+        } else {
+            NonAcsPostMetadataItem(post, onToggleFavourite = {
+                onToggleFavourite(post)
+            })
+        }
+    }
+}
+
+class PostMetadataListState {
+    var items = mutableStateListOf<PostMetadata>()
+        private set
+
+    init {
+        items.addAll(arrayListOf(post, post2))
+    }
+
+    fun onToggleFavourite(item: PostMetadata) {
+        val index = items.indexOfFirst { it.id == item.id }
+
+        if (index >= 0) {
+            items[index] = item.toggleFavorite()
+        }
+    }
+}
+
+@Composable
+private fun NonAcsPostMetadataItem(data: PostMetadata, onToggleFavourite: () -> Unit) {
+    val favouriteIcon =
+        if (data.isFavourite) Icons.Default.Favorite else Icons.Default.FavoriteBorder
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {},
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            PostMetaData(data)
+        }
+        IconButton(onClick = onToggleFavourite) {
+            Icon(
+                imageVector = favouriteIcon,
+                contentDescription = stringResource(R.string.favourite)
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun AcsPostMetadataItem(data: PostMetadata, onToggleFavourite: () -> Unit) {
+    // 1. define action labels
+    val actionLabel = stringResource(
+        if (data.isFavourite) R.string.unfavourite else R.string.favourite
+    )
+
+    val favouriteIcon =
+        if (data.isFavourite) Icons.Default.Favorite else Icons.Default.FavoriteBorder
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = {},
+                onClickLabel = stringResource(id = R.string.cd_read_article)
+            )
+            .semantics {
+                // 2. Set explicit semantic properties using CustomAccessibilityAction
+                customActions = listOf(
+                    CustomAccessibilityAction(label = actionLabel, action = {
+                        onToggleFavourite()
+                        // 3. Return true/false if accessibility action was handled
+                        true
+                    })
+                )
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            PostMetaData(data)
+        }
+        IconButton(onClick = onToggleFavourite,
+            // 4. disable semantics properties.
+            // Talkback will not interact with this node
+            modifier = Modifier.clearAndSetSemantics { }) {
+            Icon(
+                imageVector = favouriteIcon,
+                contentDescription = stringResource(R.string.favourite)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PostMetadataItemPreview() {
+    AcsPostMetadataItem(post) {
+
+    }
+}
--- app/src/main/java/com/monzon/accesbilityapp/home/mergeelement/Merge.kt
+++ app/src/main/java/com/monzon/accesbilityapp/home/mergeelement/Merge.kt
@@ -2,8 +2,7 @@ package com.monzon.accesbilityapp.home.mergeelement
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -14,32 +13,47 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.monzon.accesbilityapp.components.Header
 import com.monzon.accesbilityapp.components.Section
+import java.util.*
 
 @Composable
 fun Merge(isAccessibilityEnabled: Boolean = false) {
-    val scrollState = rememberScrollState()
-    Column(
+    val state = remember { PostMetadataListState() }
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 16.dp)
-            .verticalScroll(scrollState),
+            .padding(top = 16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Header("Semantics")
-        Section("Merge elements")
-        PostMetadata()
+
+        item {
+            Header("Semantics")
+            Section("Merge elements")
+            PostMetadata(isAccessibilityEnabled)
+            Section("List - Custom actions")
+        }
+        postMetadataList(
+            isAccessibilityEnabled = isAccessibilityEnabled,
+            items = state.items,
+            onToggleFavourite = state::onToggleFavourite
+        )
     }
 }
 
+val post = PostMetadata(
+    date = "Jan 04",
+    readTimeMinutes = "10",
+    author = Author("Carlos Monzon"),
+)
+
+val post2 = PostMetadata(
+    date = "Feb 02",
+    readTimeMinutes = "5",
+    author = Author("John Doe"),
+)
+
 @Composable
 private fun PostMetadata(isAccessibilityEnabled: Boolean = false) {
-    val metadata = remember {
-        PostMetadata(
-            date = "Jan 04",
-            readTimeMinutes = "10",
-            Author("Carlos Monzon")
-        )
-    }
+    val metadata = remember { post }
     if (isAccessibilityEnabled) {
         AcsPostMetadata(metadata = metadata)
     } else {
@@ -49,7 +63,7 @@ private fun PostMetadata(isAccessibilityEnabled: Boolean = false) {
 
 @Composable
 private fun NonAcsPostMetadata(metadata: PostMetadata) {
-    Row(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+    Row(modifier = Modifier.padding(end = 16.dp)) {
         PostMetaData(metadata = metadata)
     }
 }
@@ -59,14 +73,15 @@ private fun AcsPostMetadata(metadata: PostMetadata) {
     // Merge elements below for accessibility purposes
     Row(modifier = Modifier
         .semantics(mergeDescendants = true) {}
-        .padding(start = 16.dp, end = 16.dp)) {
+        .padding(end = 16.dp)) {
         PostMetaData(metadata = metadata)
     }
 }
 
 @Composable
-private fun PostMetaData(metadata: PostMetadata) {
+fun PostMetaData(metadata: PostMetadata) {
     Image(
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
         imageVector = Icons.Filled.AccountCircle,
         contentDescription = null // decorative
     )
@@ -75,12 +90,17 @@ private fun PostMetaData(metadata: PostMetadata) {
         Text("${metadata.date} • ${metadata.readTimeMinutes} min read")
     }
 }
-
 data class PostMetadata(
+    val id: UUID = UUID.randomUUID(),
     val date: String,
     val readTimeMinutes: String,
-    val author: Author
-)
+    val author: Author,
+    val isFavourite: Boolean = false
+) {
+    fun toggleFavorite(): PostMetadata {
+        return this.copy(isFavourite = !this.isFavourite)
+    }
+}
 
 data class Author(
     val name: String
